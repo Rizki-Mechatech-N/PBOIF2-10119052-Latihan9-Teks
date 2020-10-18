a6a19cc33fb6caa0ecd1a1bdef61295c88a4e254
Kiki <Kiki@Kiki-PC>
2020-10-18 11:20:33 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan9/teks/PBOIF210119052Latihan9Teks.java
@@ -0,0 +1,25 @@
+package pboif2.pkg10119052.latihan9.teks;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * Teks
+ * 
+ */
+public class PBOIF210119052Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
